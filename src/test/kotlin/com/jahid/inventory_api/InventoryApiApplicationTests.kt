3d77a62e340aa6e550e6f18a4f4ab9e158968e19
JAHID KHAN <jkwonderlover@gmail.com>
2026-04-01 13:59:43 +0200

    removed annotation @SpringBootTest because I'm using Mockito and Unit testing, so no need of whole Spring configuration

--- src/test/kotlin/com/jahid/inventory_api/InventoryApiApplicationTests.kt
+++ src/test/kotlin/com/jahid/inventory_api/InventoryApiApplicationTests.kt
@@ -1,9 +1,8 @@
 package com.jahid.inventory_api
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+
 class InventoryApiApplicationTests {
 
 	@Test
